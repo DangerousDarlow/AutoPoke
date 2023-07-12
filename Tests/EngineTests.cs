f4ac74e16ff1c55423ca5b48f679d1a01bba3f87
Nick Darlow <dangerous.darlow@gmail.com>
2023-07-12 08:45:36 +0100

    Add tests for game start

--- Tests/EngineTests.cs
+++ Tests/EngineTests.cs
@@ -8,6 +8,7 @@ namespace Tests;
 public class EngineTests
 {
     private IEngine _engine = null!;
+    private IServer _server = null!;
     private MockZeroMq _mockZeroMq = null!;
     private Dictionary<Guid, Player> _players = null!;
 
@@ -21,7 +22,8 @@ public class EngineTests
             .AddSingleton<IServer>(provider =>
             {
                 var mockZeroMq = provider.GetService<MockZeroMq>()!;
-                return mockZeroMq.CreateServer();
+                _server = mockZeroMq.CreateServer();
+                return _server;
             })
             .AddSingleton<IClient>(provider =>
             {
@@ -66,7 +68,7 @@ public class EngineTests
         var player = CreatePlayer("Player");
         Server.Handle(new JoinRequest {PlayerId = player.Id, PlayerName = player.Name});
 
-        Assert.That(Server.SentToSingleClient, Has.Count.EqualTo(1), "Server has not sent response");
+        Assert.That(Server.SentToSingleClient, Has.Count.EqualTo(1), "Engine has not sent response");
         var joinResponse = Server.SentToSingleClient[0] as JoinResponse;
         Assert.That(joinResponse, Is.Not.Null);
         Assert.That(joinResponse!.Status, Is.EqualTo(JoinResponseStatus.Success));
@@ -84,7 +86,7 @@ public class EngineTests
         var player = CreatePlayer("Player");
         Server.Handle(new JoinRequest {PlayerId = player.Id, PlayerName = player.Name});
 
-        Assert.That(Server.SentToSingleClient, Has.Count.EqualTo(_engine.Configuration.MaxPlayers + 1), "Server has not sent response");
+        Assert.That(Server.SentToSingleClient, Has.Count.EqualTo(_engine.Configuration.MaxPlayers + 1), "Engine has not sent response");
         var joinResponse = Server.SentToSingleClient.Last() as JoinResponse;
         Assert.That(joinResponse, Is.Not.Null);
         Assert.That(joinResponse!.Status, Is.EqualTo(JoinResponseStatus.FailureEngineFull));
@@ -100,7 +102,7 @@ public class EngineTests
 
         Server.Handle(new BeginSession {Games = 1});
 
-        Assert.That(Server.SentToAllClients, Has.Count.EqualTo(2), "Server has not sent responses");
+        Assert.That(Server.SentToAllClients, Has.Count.EqualTo(2), "Engine has not sent responses");
 
         var sessionStarted = Server.SentToAllClients[0] as SessionStarted;
         Assert.That(sessionStarted, Is.Not.Null);
@@ -124,9 +126,43 @@ public class EngineTests
         // Then engine does not respond
 
         Server.Handle(new BeginSession {Games = 1});
-        Assert.That(Server.SentToAllClients, Has.Count.EqualTo(2), "Server has not sent responses");
+        Assert.That(Server.SentToAllClients, Has.Count.EqualTo(2), "Engine has not sent responses");
 
         Server.Handle(new BeginSession {Games = 1});
         Assert.That(Server.SentToAllClients, Has.Count.EqualTo(2), "Additional responses sent");
+    }
+
+    [Test]
+    public void Game_can_be_started()
+    {
+        // When engine receives BeginGame and origin is engine
+        // Then engine responds to all clients with GameStarted
+
+        CreateAndJoinMaximumPlayers();
+
+        var envelope = Envelope.CreateFromEvent(new BeginGame());
+        envelope.Origin = _server.Id;
+        Server.Handle(envelope);
+
+        Assert.That(Server.SentToAllClients, Has.Count.EqualTo(1), "Engine has not sent responses");
+
+        var gameStarted = Server.SentToAllClients[0] as GameStarted;
+        Assert.That(gameStarted, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(gameStarted!.Game.Sequence, Is.EqualTo(1));
+            Assert.That(gameStarted.Game.Players, Is.EquivalentTo(_players.Values));
+            Assert.That(gameStarted.Game.StartingStack, Is.EqualTo(_engine.Configuration.StartingStack));
+        });
+    }
+
+    [Test]
+    public void BeginGame_is_ignored_if_origin_is_not_engine()
+    {
+        // When engine receives BeginGame and origin is not engine
+        // Then BeginGame is ignored
+
+        Server.Handle(new BeginGame());
+        Assert.That(Server.SentToAllClients, Is.Empty, "Engine has not ignored BeginGame");
     }
 }
