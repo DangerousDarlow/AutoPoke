59741ba07f8daef673048db1204f0ef3fe9a86b8
Nick Darlow <dangerous.darlow@gmail.com>
2023-07-18 09:07:46 +0100

    Client handles HandStarted events

---
+++ Client/PlayerEventHandlers/HandStartedHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Model;
+
+namespace Client.PlayerEventHandlers;
+
+// Class is used via reflection
+// ReSharper disable once UnusedType.Global
+public class HandStartedHandler : IPlayerEventHandler
+{
+    private readonly ILogger<HandStartedHandler> _logger;
+
+    public HandStartedHandler(ILogger<HandStartedHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public IPlayer Player { get; set; } = null!;
+
+    public Type TypeHandled => typeof(HandStarted);
+
+    public void HandleEvent(IEvent @event)
+    {
+        var handStarted = (HandStarted) @event;
+        Player.Hand = handStarted.Hand;
+        _logger.LogDebug("Hand '{HandId}' started", handStarted.Hand.Id);
+    }
+}
