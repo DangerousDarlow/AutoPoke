2c389e4cac1307ae287084c03b7abd2c860efc17
Nick Darlow <dangerous.darlow@gmail.com>
2023-07-18 09:07:21 +0100

    Add script to open powershell processes

---
+++ Shells.ps1
@@ -0,0 +1,6 @@
+﻿$dirs = "Server\bin\Debug\net7.0\", "Client\bin\Debug\net7.0\", "Client\bin\Debug\net7.0\", "Scripts\BeginSession\bin\Debug\net7.0\"
+
+foreach ($dir in $dirs)
+{
+    Start-Process powershell.exe -ArgumentList "-NoExit", "cd $dir"
+}
